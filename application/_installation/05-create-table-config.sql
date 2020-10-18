8ef89066d2cd0647661ce2cdcc7fa889f65a0509
cristopher castro <cristophernic@live.cl>
2020-10-18 16:50:57 -0300

    Error

--- application/_installation/05-create-table-config.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE IF NOT EXISTS `huge`.`config` (
- `config_id` int(11) unsigned NOT NULL AUTO_INCREMENT,
- `config_name` text NOT NULL,
- `config_key` text NOT NULL,
- PRIMARY KEY (`config_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci COMMENT='configuración para crecimientofetal.cl';
